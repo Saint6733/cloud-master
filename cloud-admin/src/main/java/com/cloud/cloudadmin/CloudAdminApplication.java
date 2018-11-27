2f0f26c41b714060735832de8048bad6b12ccc72
Saint6733 <673358025@qq.com>
2018-11-27 14:49:16 +0800

    调整maven依赖，添加cloud-admin模块

---
+++ cloud-admin/src/main/java/com/cloud/cloudadmin/CloudAdminApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.cloudadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAdminApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAdminApplication.class, args);
+	}
+}
