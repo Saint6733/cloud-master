03ba596796ba38fd43ba5382cbb6c697930364ed
Saint6733 <673358025@qq.com>
2018-11-27 12:57:06 +0800

    初始化项目，基础配置

--- cloud-store/src/main/java/com/cloud/store/StoreApplication.java
+++ cloud-store/src/main/java/com/cloud/store/StoreApplication.java
@@ -1,7 +1,12 @@
 package com.cloud.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 类名称：StoreApplication<br>
@@ -12,7 +17,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class StoreApplication {
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	/**
+	 * 获取所有服务
+	 */
+	@RequestMapping("/services")
+	public Object services() {
+		return discoveryClient.getServices();
+	}
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "Hello World";
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class,args);
