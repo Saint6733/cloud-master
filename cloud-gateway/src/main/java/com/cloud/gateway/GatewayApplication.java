1fcf92999c18dc149503089350404e49922c017d
Saint6733 <673358025@qq.com>
2018-11-28 14:49:28 +0800

    针对Gateway Start 默认WebFlux调整整体依赖

--- cloud-gateway/src/main/java/com/cloud/gateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/com/cloud/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 类名称：GatewayApplication<br>
@@ -14,6 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"org.springframework.http.codec"})
 public class GatewayApplication {
 	
 	public static void main(String[] args) {
