5c7dca8e60773bd40b744769f4b01d2f44b856b3
Saint6733 <673358025@qq.com>
2018-11-29 09:47:57 +0800

    Spring Boot Admin的加入，以注册中心的方式监控各个服务实例

--- cloud-admin/src/main/java/com/cloud/cloudadmin/CloudAdminApplication.java
+++ cloud-admin/src/main/java/com/cloud/cloudadmin/CloudAdminApplication.java
@@ -1,9 +1,18 @@
 package com.cloud.cloudadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ *
+ * @author 冯亚鹏
+ * @version	1.0.0
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class CloudAdminApplication {
 	
 	public static void main(String[] args) {
