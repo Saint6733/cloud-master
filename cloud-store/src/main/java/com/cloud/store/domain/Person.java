cc478a67e37d8bf855c2e25f35631cd1fb3c814e
shiqianghui <squirrelzt@126.com>
2018-12-05 18:14:38 +0800

    mongodb操作

---
+++ cloud-store/src/main/java/com/cloud/store/domain/Person.java
@@ -0,0 +1,20 @@
+package com.cloud.store.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+/**
+ * 类名称:Person
+ * 类描述:测试实体类
+ * @author shiqianghui
+ * @date 2018-12-05
+ */
+@Data
+public class Person {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
